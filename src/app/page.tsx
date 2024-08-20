a591bcd8db262d4e3bb27536beb44053b6a23d8f
ahsan3797@gmail.com <ahsan3797@gmail.com>
2024-08-20 21:58:00 +0500

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,8 @@
+'use client'
+import Image from "next/image";
+import ResizablePanelsLayout from "@/components/ResizablePanelsLayout";
+export default function Home() {
+  return (
+    <ResizablePanelsLayout />
+  );
+}
